75ddea340824172460410c599dead0fe83aea94b
jbros17a <107471942+jbros17a@users.noreply.github.com>
2023-01-08 10:38:19 +0900

    Code upload #1

---
+++ src/views.py
@@ -0,0 +1,53 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .models import Task
+
+class TaskList(LoginRequiredMixin, ListView):
+    model = Task
+    template_name = 'task_list.html'
+    context_object_name = 'tasks'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(is_completed=False).count()
+        search_input = self.request.GET.get('search-form') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(title__icontains=search_input)
+        context['search_input'] = search_input
+        return context
+
+
+class TaskDetail(LoginRequiredMixin, DetailView):
+    model = Task
+    template_name = 'task_detail.html'
+    context_object_name = 'task'
+
+
+class CreateTask(LoginRequiredMixin, CreateView):
+    model = Task
+    template_name = 'task_form.html'
+    fields = ['title', 'description', 'is_completed']
+    success_url = reverse_lazy('tasks:main')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(CreateTask, self).form_valid(form)
+
+
+class UpdateTask(LoginRequiredMixin, UpdateView):
+    model = Task
+    template_name = 'task_form.html'
+    fields = ['title', 'description', 'is_completed']
+    success_url = reverse_lazy('tasks:main')
+
+
+class DeleteTask(LoginRequiredMixin, DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks:main')
+    template_name = 'task_delete_confirm.html'
